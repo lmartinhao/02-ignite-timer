08b5b4459e3f6b01f608b238db10a58290583ea9
lola martinhao <lola.martinhao@gmail.com>
2023-01-23 18:31:57 -0300

    adding styled components to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import { Button } from './components/Button'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-    </div>
+    <>
+      <Button variant='primary' />
+      <Button variant='secondary' />
+      <Button variant='success' />
+      <Button variant='danger' />
+      <Button />
+    </>
   )
 }
