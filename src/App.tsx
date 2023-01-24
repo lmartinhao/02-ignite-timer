2c18bc1fbd8f7858f4e367154cf8537dfd9cbccc
lola martinhao <lola.martinhao@gmail.com>
2023-01-24 01:14:11 -0300

    adding types file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
 
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant='primary' />
       <Button variant='secondary' />
       <Button variant='success' />
       <Button variant='danger' />
       <Button />
-    </>
+    </ThemeProvider>
   )
 }
