62a6739c43d8ad1a13b475892f3897645445644a
lola martinhao <lola.martinhao@gmail.com>
2023-01-24 16:51:52 -0300

    adding eslint and fixing errors

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,12 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant='primary' />
-      <Button variant='secondary' />
-      <Button variant='success' />
-      <Button variant='danger' />
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
       <Button />
 
       <GlobalStyle />
