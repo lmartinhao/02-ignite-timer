ecd20dad4567896d986a8decc9eec32280f640d0
lola martinhao <lola.martinhao@gmail.com>
2023-02-02 17:18:54 -0300

    fixing desestructure issue

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -56,7 +56,7 @@ export function CyclesContextProvider({
     },
   )
 
-  const { cycles, activeCycleId } = cyclesState
+  const { cycles, activeCycleId } = cyclesState || {}
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   const [amountSecondsPast, setAmountSecondsPast] = useState(() => {
